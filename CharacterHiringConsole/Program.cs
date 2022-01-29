a183234706ee6f76318c928cbed952f13e6613ba
Richard Hannah <richard.hannah1974@gmail.com>
2022-01-29 23:58:53 +0000

    remove testassembly and refactor a bit

--- CharacterHiringConsole/Program.cs
+++ CharacterHiringConsole/Program.cs
@@ -3,21 +3,17 @@ using CharacterHiringConsole;
 using Microsoft.Extensions.Logging;
 using Ninject;
 using Ninject.Modules;
-using TestAssembly;
 
 public class Program
 {
+    private static ILoggerFactory loggerFactory;
+    private static IKernel kernel;
+
     public static void Main(string[] args)
     {
         //configure console logging
-        using var loggerFactory = LoggerFactory.Create(builder =>
-        {
-            builder
-                .AddFilter("Microsoft", LogLevel.Warning)
-                .AddFilter("System", LogLevel.Warning)
-                .AddFilter("NonHostConsoleApp.Program", LogLevel.Debug)
-                .AddConsole();
-        });
+        ConfigureLogging();
+        ConfigureDI();
 
         ILogger logger = loggerFactory.CreateLogger<Program>();
         logger.LogDebug("Starting application");
@@ -29,17 +25,7 @@ public class Program
             NameLists = DefaultConfigProvider.GetValidCharacterNameLists()
         };
 
-        var kernel = new StandardKernel();
-
-        var assemblies = AppDomain.CurrentDomain.GetAssemblies()
-            .Where(a => a.GetExportedTypes().Any(t => t.BaseType == typeof(NinjectModule)));
-
-        kernel.Load(assemblies);
-
         var characterMarket = kernel.Get<ICharacterMarket>();
-        var class1 = kernel.Get<IClass1>();
-
-        class1.HelloWorld();
 
         characterMarket.Configuration = config;
         characterMarket.Stock();
@@ -48,5 +34,27 @@ public class Program
         characters.ForEach(c => Console.WriteLine(c.FullName));
 
         logger.LogDebug("All done!");
+    }
+
+    private static void ConfigureDI()
+    {
+        kernel = new StandardKernel();
+
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+            .Where(a => a.GetExportedTypes().Any(t => t.BaseType == typeof(NinjectModule)));
+
+        kernel.Load(assemblies);
+    }
+
+    private static void ConfigureLogging()
+    {
+        loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter("Microsoft", LogLevel.Warning)
+                .AddFilter("System", LogLevel.Warning)
+                .AddFilter("NonHostConsoleApp.Program", LogLevel.Debug)
+                .AddConsole();
+        });
     }
 }
